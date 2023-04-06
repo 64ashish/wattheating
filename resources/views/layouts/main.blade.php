342de74b910729b054b6735f1ba5247217bfd156
Ashish <ashish@kortaben.se>
2023-04-06 09:25:16 +0200

    added marker

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,6 +8,16 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap" rel="stylesheet">
 {{--    <link rel="stylesheet" href="https://use.typekit.net/hav8ltd.css">--}}
     <link rel="stylesheet" href="https://use.typekit.net/hav8ltd.css">
+    <script>
+        window.markerConfig = {
+            project: '642e6bc041b9c07243646b47',
+            source: 'snippet'
+        };
+    </script>
+
+    <script>
+        !function(e,r,a){if(!e.__Marker){e.__Marker={};var t=[],n={__cs:t};["show","hide","isVisible","capture","cancelCapture","unload","reload","isExtensionInstalled","setReporter","setCustomData","on","off"].forEach(function(e){n[e]=function(){var r=Array.prototype.slice.call(arguments);r.unshift(e),t.push(r)}}),e.Marker=n;var s=r.createElement("script");s.async=1,s.src="https://edge.marker.io/latest/shim.js";var i=r.getElementsByTagName("script")[0];i.parentNode.insertBefore(s,i)}}(window,document);
+    </script>
 </head>
 
 <body>
