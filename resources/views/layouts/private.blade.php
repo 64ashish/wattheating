483da3e1c5ee3d30e1250522356e4c7097703547
Ashish <ashish@kortaben.se>
2023-05-25 09:22:53 +0200

    minor fixes on private page

--- resources/views/layouts/private.blade.php
+++ resources/views/layouts/private.blade.php
@@ -8,6 +8,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap" rel="stylesheet">
     {{--    <link rel="stylesheet" href="https://use.typekit.net/hav8ltd.css">--}}
     <link rel="stylesheet" href="https://use.typekit.net/hav8ltd.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script>
     <script>
         window.markerConfig = {
             project: '642e6bc041b9c07243646b47',
@@ -42,11 +43,11 @@
     </div>
     {{--  Ribbon large screen --}}
     <div class="hidden lg:block text-sm">
-            <div class="h-10  text-center bg-secondary-900 text-white flex items-center justify-center gap-x-3">
-                <span class="">ALLTID FRI FRAKT INOM SVERIGE</span>
-                <span>|</span>
-                <span class="font-semibold">BETALA SÄKERT MED</span>
-            </div>
+        <div class="h-10  text-center bg-secondary-900 text-white flex items-center justify-center gap-x-3">
+            <span class="">ALLTID FRI FRAKT INOM SVERIGE</span>
+            <span>|</span>
+            <span class="font-semibold flex gap-4 items-center">BETALA SÄKERT MED  <img class="w-14 h-3" src="/images/logo_white.png"></span>
+        </div>
     </div>
 
     <x-header-menu></x-header-menu>
@@ -55,5 +56,38 @@
 {{ $slot }}
 
 <x-footer-block></x-footer-block>
+<script>
+    gsap.set(".main_animation_conteiner #lastbilForkliftSvg",{opacity:1});
+
+
+    gsap.to('#lastbilForkliftSvg #gFLLump',{duration:0.2,repeat:-1,yoyo:true,opacity:0,ease:'power1.inOut'})
+
+    gsap.timeline({repeat:-1,repeatDelay:0.5})
+        .to('#lastbilForkliftSvg #gFLW > *',{duration:0.5,transformOrigin:'50% 50%',repeat:1,rotation:360,ease:'none'})
+        .to('#lastbilForkliftSvg #gFL',{duration:1,x:30,ease:'none'},'<')
+        .to('#lastbilForkliftSvg #gFLfork',{duration:0.8,y:-30,ease:'none'},'>')
+        .to('#lastbilForkliftSvg #gFLfork',{duration:0.8,y:0,ease:'none'},'>')
+        .to('#lastbilForkliftSvg #gFLW > *',{duration:0.5,transformOrigin:'50% 50%',repeat:1,rotation:0,ease:'none'})
+        .to('#lastbilForkliftSvg #gFL',{duration:1,x:0,ease:'none'},'<')
+
+</script>
+
+<script>
+    gsap.set(".main_animation_conteiner #lastbilTruckSvg",{opacity:1});
+
+    gsap.set('#lastbilTruckSvg #gTVin > *',{strokeDasharray:32,strokeDashoffset:32*3})
+    gsap.to('#lastbilTruckSvg #gTVin > *:nth-child(1)',{duration:0.5,repeat:-1,strokeDashoffset:32,ease:'power1.inOut'})
+    gsap.to('#lastbilTruckSvg #gTVin > *:nth-child(2)',{duration:0.5,repeat:-1,delay:0.1,strokeDashoffset:32,ease:'power1.inOut'})
+    gsap.to('#lastbilTruckSvg #gTW > *',{duration:0.5,transformOrigin:'50% 50%',repeat:-1,rotation:360,ease:'none'})
+    gsap.to('#lastbilTruckSvg #gTAll',{duration:2,repeat:-1,x:5,yoyo:true,ease:'power1.inOut'})
+
+    gsap.timeline({repeat:-1,repeatDelay:0.5})
+        .to('#lastbilTruckSvg #gT',{duration:0.3,transformOrigin:'70% 50%',rotation:-1,ease:'none'})
+        .to('#lastbilTruckSvg #gT',{duration:0.5,rotation:1,ease:'none'})
+        .to('#lastbilTruckSvg #gT',{duration:0.3,rotation:0,ease:'sine.out'})
+
+
+
+</script>
 </body>
 </html>
--- resources/views/pages/private-home.blade.php
+++ resources/views/pages/private-home.blade.php
@@ -23,19 +23,148 @@
 
     <div class="bg-alabaster-50 grid grid-cols-1 lg:grid-cols-2 px-1">
         <div class="text-4xl text-primary-500 font-semibold px-12 pt-12 lg:px-24 lg:py-20">
-            Med mer än 50.000 radiatorer i lager vet vi vad vi pratar om!
+            <p class="mb-6">
+                Med mer än 50.000 radiatorer i lager vet vi vad vi pratar om!
+            </p>
+            <div class="main_animation_conteiner forklift_holder">
+                <!--?xml version="1.0" encoding="utf-8"?-->
+                <!-- Generator: Adobe Illustrator 24.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+                <svg version="1.1" id="lastbilForkliftSvg" class=" h-20 w-full" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="475px" height="50px" viewBox="0 0 475 50" enable-background="new 0 0 475 50" xml:space="preserve" style="opacity: 1;">
+                    <g>
+                        <path fill="none" stroke="#00B5CB" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+                            M1.35,48.65h51.13"></path>
+                        <path fill="none" stroke="#00B5CB" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+                            M95,49h379"></path>
+                        <path fill="none" stroke="#00B5CB" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+                            M67.48,48.65h10.57"></path>
+                    </g>
+                                    <g id="gFL" data-svg-origin="54.25 1.350000023841858" transform="matrix(1,0,0,1,10.05,0)" style="transform-origin: 0px 0px;">
+                                        <path fill="none" stroke="#00B5CB" stroke-width="1.4" stroke-linecap="round" stroke-miterlimit="10" d="M54.25,41.14v4.59h2.16
+                        c0-2.84,2.29-5.14,5.11-5.14c2.82,0,5.11,2.3,5.11,5.14h12.63c0.74-4.74,4.71-5.23,4.71-5.23s-0.62-21.75-7.89-31.26L60.94,8.44
+                        v20.27c-3.7,0.01-6.69,3.03-6.69,6.76"></path>
+                                        <path fill="none" stroke="#00B5CB" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+                        M83.97,40.51h3.33V6.63h-3.36v12.34"></path>
+                                        <path fill="none" stroke="#00B5CB" stroke-width="1.4" stroke-linecap="round" stroke-miterlimit="10" d="M79.22,24.1
+                        c-0.92-4.29-2.41-9.45-4.58-12.14l-11.09-0.72v17.47l9.19,1.3"></path>
+                                        <path fill="none" stroke="#00B5CB" stroke-width="1.4" stroke-linecap="round" stroke-miterlimit="10" d="M72.73,30l7.68,1.08
+                        c0,0-0.14-1.19-0.44-3"></path>
+                                        <g id="gFLfork" data-svg-origin="87.30000305175781 38.279998779296875" transform="matrix(1,0,0,1,0,0)" style="transform-origin: 0px 0px;">
+                                            <path fill="none" stroke="#00B5CB" stroke-width="1.4" stroke-linecap="round" stroke-miterlimit="10" d="M87.3,40.51h17.13"></path>
+
+                                            <path fill="none" stroke="#00B5CB" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+                            M87.3,38.28h14.45"></path>
+                                        </g>
+                                        <path fill="none" stroke="#00B5CB" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+                        M63.51,8.52l0.06-1.26c0.03-0.69,0.62-1.23,1.31-1.2c0.69,0.03,1.22,0.62,1.19,1.32l-0.06,1.26"></path>
+                                        <g id="gFLLump" style="opacity: 0.7887;">
+
+                                            <path fill="none" stroke="#00B5CB" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+                            M61.05,3.32l1.44,1.77"></path>
+
+                                            <path fill="none" stroke="#00B5CB" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+                            M68.2,3.32l-1.44,1.77"></path>
+
+                                            <path fill="none" stroke="#00B5CB" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+                            M64.79,1.35l0.03,3.11"></path>
+                                        </g>
+                                        <g id="gFLW">
+                                            <g data-svg-origin="84.80999374389648 46.00000190734863" style="transform-origin: 0px 0px;" transform="matrix(-0.48175,-0.87631,0.87631,-0.48175,85.35695,142.48035)">
+                                                <path fill="none" stroke="#00B5CB" stroke-width="1.4" stroke-linecap="round" stroke-miterlimit="10" d="M85,49
+                                c1.66,0,3-1.34,3-3s-1.34-3-3-3s-3,1.34-3,3S83.34,49,85,49z"></path>
+                                                <circle fill="none" cx="84.81" cy="46" r="4.19"></circle>
+                                            </g>
+                                            <g data-svg-origin="61.63999938964844 46.49999809265137" style="transform-origin: 0px 0px;" transform="matrix(-0.48175,-0.87631,0.87631,-0.48175,50.58666,122.91712)">
+                                                <path fill="none" stroke="#00B5CB" stroke-width="1.4" stroke-linecap="round" stroke-miterlimit="10" d="M61.5,49
+                                c1.38,0,2.5-1.12,2.5-2.5S62.88,44,61.5,44S59,45.12,59,46.5S60.12,49,61.5,49z"></path>
+                                                <circle fill="none" cx="61.64" cy="46.5" r="3.83"></circle>
+                                            </g>
+                                        </g>
+                                    </g>
+                </svg>
+            </div>
+
         </div>
         <div class="px-12 py-12 text-xl font-semibold lg:px-24 lg:py-20">
-            Betala tryggt och säkert med
+           <p class="mb-6">
+               Betala tryggt och säkert med
             <img class="w-36 pt-5" src="/images/logo_black.png">
+           </p>
+            <div class="main_animation_conteiner truck_holder ">
+                <!--?xml version="1.0" encoding="utf-8"?-->
+                <!-- Generator: Adobe Illustrator 24.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+                <svg version="1.1" id="lastbilTruckSvg" class=" h-20 w-full" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="643px" height="64.56px" viewBox="0 0 643 64.56" enable-background="new 0 0 643 64.56" xml:space="preserve" style="opacity: 1;">
+                        <g id="gTAll" data-svg-origin="132.5 5.779999732971191" style="transform-origin: 0px 0px;" transform="matrix(1,0,0,1,3.7255,0)">
+                            <g id="gT" data-svg-origin="250.10422973632814 33.5731782913208" style="transform-origin: 0px 0px;" transform="matrix(0.99999,-0.00447,0.00447,0.99999,-0.14757,1.1183)">
+                                <g>
+
+                                    <path fill="none" stroke="#010202" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+                                        M268.5,5.78h-98v36h98V5.78z"></path>
+
+                                    <path fill="none" stroke="#010202" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+                                        M284.56,30.36l10.94,2.41c0,0-0.13-9.23-3.21-12.83l-11.79-0.17v9"></path>
+
+                                    <path fill="none" stroke="#010202" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+                                        M271.29,39.67v5.51H176.5v5.6l22.55,0.73c1.08-1.7,2.94-2.82,5.07-2.82c2.26,0,4.23,1.27,5.27,3.16l4.9,0.16
+                                        c1.01-1.97,3.03-3.31,5.35-3.31c2.46,0,4.58,1.51,5.52,3.67l53.81,1.74c0.37-3.05,2.92-5.41,6-5.41c3.21,0,5.83,2.56,6.03,5.8
+                                        l8.94,0.29c0,0,3.41-29.04-6.51-39.2l-22.13-4.8v18.98"></path>
+
+                                    <path fill="none" stroke="#010202" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+                                        M283.5,42.78h4.22l3.77,2h9"></path>
+
+                                    <path fill="none" stroke="#010202" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+                                        M271.5,45.28h7l2-1.5"></path>
+
+                                    <path fill="none" stroke="#010202" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+                                        M280.5,19.78h-5v8.1l5,0.9"></path>
+                                </g>
+                                <g id="gTW">
+                                    <g data-svg-origin="285.00001525878906 55.04000282287598" style="transform-origin: 0px 0px;" transform="matrix(-0.89941,-0.43712,0.43712,-0.89941,517.27279,229.12274)">
+
+                                        <path fill="none" stroke="#010202" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+                                            M285,58.78c1.93,0,3.5-1.57,3.5-3.5s-1.57-3.5-3.5-3.5s-3.5,1.57-3.5,3.5S283.07,58.78,285,58.78z"></path>
+                                        <circle fill="none" cx="285" cy="55.04" r="4.74"></circle>
+                                    </g>
+                                    <g data-svg-origin="219.74000549316406 55.279998779296875" style="transform-origin: 0px 0px;" transform="matrix(-0.89941,-0.43712,0.43712,-0.89941,393.21237,201.05213)">
+
+                                        <path fill="none" stroke="#010202" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+                                            M220,58.78c1.93,0,3.5-1.57,3.5-3.5s-1.57-3.5-3.5-3.5s-3.5,1.57-3.5,3.5S218.07,58.78,220,58.78z"></path>
+                                        <circle fill="none" cx="219.74" cy="55.28" r="4.89"></circle>
+                                    </g>
+                                    <g data-svg-origin="204.3000030517578 55.279998779296875" style="transform-origin: 0px 0px;" transform="matrix(-0.89941,-0.43712,0.43712,-0.89941,363.88548,194.303)">
+
+                                        <path fill="none" stroke="#010202" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+                                            M204,58.78c1.93,0,3.5-1.57,3.5-3.5s-1.57-3.5-3.5-3.5s-3.5,1.57-3.5,3.5S202.07,58.78,204,58.78z"></path>
+                                        <circle fill="none" cx="204.3" cy="55.28" r="5"></circle>
+                                    </g>
+                                </g>
+                                <g id="gTVin">
+
+                                    <path fill="none" stroke="#010202" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+                                        M163.5,41.78h-31" style="stroke-dashoffset: 55px; stroke-dasharray: 32;"></path>
+
+                                    <path fill="none" stroke="#010202" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+                                        M163.5,36.78h-23" style="stroke-dashoffset: 78px; stroke-dasharray: 32;"></path>
+                                </g>
+                            </g>
+                        </g>
+                                        <g>
+                                            <path fill="none" stroke="#292929" stroke-width="1.4" stroke-linecap="round" d="M295,58.78l347,0"></path>
+                                            <path fill="none" stroke="#010202" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+                            M1,58.78h81"></path>
+                                            <path fill="none" stroke="#010202" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+                            M229.5,58.78h46"></path>
+                                            <path fill="none" stroke="#010202" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+                            M102,58.78h94.5"></path>
+                                        </g>
+                    </svg>
+            </div>
         </div>
     </div>
-
     <div class=" lg:grid-cols-7">
         <div class="lg:col-span-4 bg-alabaster-50 lg:bg-white flex justify-center px-16 pt-24 pb-10">
             <img class="pt-5 w-full max-w-2xl" src="/images/svensk-standard.png">
         </div>
-        <div class="py-20 px-12 lg:py-36 lg:pl-24 lg:pr-11 lg:col-span-3  flex justify-center flex-col
+        <div class="py-20 px-12 lg:pb-36 lg:pl-24 lg:pr-11 lg:col-span-3  flex justify-center flex-col
             bg-primary-500  lg:bg-white items-center">
             <div class="text-white lg:text-secondary-900 max-w-2xl lg:text-center">
                 <p class="text-3xl lg:text-4xl font-semibold mb-7 ">Det du inte visste du behövde. Förrän nu!</p>
@@ -122,11 +251,10 @@
 
     <div class="py-44 bg-primary-500 flex flex-col justify-center items-center">
 
-            <div class="text-4xl text-white font-semibold pb-10">
+            <div class="text-4xl text-white font-semibold pb-20 text-center">
                 Prenumerera på vårt nyhetsbrev!
             </div>
             <a href="=" class=" text-white font-bold px-9 py-4 rounded-full border-2 border-white">Klicka här </a>
-
     </div>
 
 </x-private-layout>
